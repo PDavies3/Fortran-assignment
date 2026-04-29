f2d34fdd6fdbbb3ac2cb8df87ac37422df3881cd
GerrardBoakyeAgyeman <gerrardboakyeagyeman@gmail.com>
2026-04-29 22:30:34 +0000

    Fortran.f90
    Updated Mid-Sem Code

--- MET253_Assignment_repo/Fortran.f90
+++ MET253_Assignment_repo/Fortran.f90
@@ -1,19 +1,33 @@
 !Index Number - 6308524
 Program Quiz
- Implicit None
- Integer, Dimension :: Student,Score
- Integer :: i
- Student(1)=
- Student(2)=2
- Student(3)=3
- Student(4)=4
- Student(5)=5
- Student(6)=6
- Student(7)=7
- Student(8)=8
- Student(9)=9
- Student(10)=10
- 
- Print*
- 
- 
+ IMPLICIT NONE
+
+ INTEGER, DIMENSION(10) :: scores = (/85, 62, 45, 91, 38, 74, 55, 88, 61, 47/)
+ INTEGER :: i, score
+ CHARACTER(LEN=1) :: grade
+ CHARACTER(LEN=11) :: remark
+
+ PRINT "(A6, A10, A8, A13)", "Std No", "Score", "Grade", "Remark"
+
+   DO i = 1, 10
+     score = scores(i)
+
+        IF (score >= 80 .AND. score <= 100) THEN
+            grade  = "A"
+            remark = "Distinction"
+        ELSE IF (score >= 60 .AND. score <= 79) THEN
+            grade  = "B"
+            remark = "Credit"
+        ELSE IF (score >= 40 .AND. score <= 59) THEN
+            grade  = "C"
+            remark = "Pass"
+        ELSE
+            grade  = "F"
+            remark = "Fail"
+        END IF
+
+        PRINT "(I6, I10, A8, A13)", i, score, grade, remark
+
+   END DO
+    
+END PROGRAM Quiz 
