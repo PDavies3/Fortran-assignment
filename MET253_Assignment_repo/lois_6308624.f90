9447eb0f0ae2935b83e4e527477ebbac9738d0f0
loisboakyegyamfi7-cpu <loisboakyegyamfi7@gmail.com>
2026-04-30 15:30:11 +0000

    Update lois_6308624.f90

--- MET253_Assignment_repo/lois_6308624.f90
+++ MET253_Assignment_repo/lois_6308624.f90
@@ -1,22 +1,23 @@
-PROGRAM lois_6308624
+PROGRAM maths_class 
 IMPLICIT NONE
-INTEGER :: (x,10)
-INTEGER :: scores(85,62,45,91,38,74,55,88,61,47)
-student(1)=85
-student(2)=62
-student(3)=45
-student(5)=38
-student(6)=74
-student(7)=55
-student(8)=88
-student(9)=61
-student(10)=47
-PRINT*,'A'
-IF 79>S<60
-PRINT*,'B'
-IF 40>S<59
-PRINT*,'C'
-IF 39>S<0
-PRINT*,'F
-END lois_6308624
-
+INTEGER,DIMENSION(10)::Scores=(/85,62,45,91,38,74,55,88,61,47/)
+CHARACTER(1),DIMENSION(10):: Grades
+INTEGER::i
+CHARACTER(LEN=2)::Grade
+CHARACTER(LEN=11)::Remark
+PRINT*,'Student report'
+PRINT*,'Student No. Score Grade Remark'
+DO i=1 , 10
+IF (Scores(i) >= 80) THEN
+Grade = 'A'; Remark = 'Distinction'
+ELSE IF (Scores(i) >= 60) THEN 
+Grade = 'B'; Remark = 'Credit'
+ELSE IF (Scores(i) >= 40 ) THEN
+Grade = 'C'; Remark = 'Pass'
+ELSE 
+Grade = 'F'; Remark = 'Fail'
+END IF 
+WRITE( *,100) i, Scores(i), Grade, Remark
+100 FORMAT( I3, 6X, I3, 6X, A2, 2X, A12)
+END DO
+END PROGRAM maths_class
