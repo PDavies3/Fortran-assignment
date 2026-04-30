933d39f94362b3d9e8c8506fb4b7fa85b94b0fc0
vicsizze1 <armahvictor678@gmail.com>
2026-04-30 19:55:08 +0000

    Update victor_6307324.f90

--- MET253_Assignment_repo/victor_6307324.f90
+++ MET253_Assignment_repo/victor_6307324.f90
@@ -1,30 +1,48 @@
-program grade
+program grading_system
 !Victor Paakwesi Armah 
 !6307324
 implicit none
-real, dimension(10) :: s
-integer :: i
-(s)=85
-(s)=62
-(s)=45
-(s)=91
-(s)=38
-(s)=74
-(s)=55
-(s)=88
-(s)=61
-(s)=47
+
+!(a)Data declaration
+integer, dimension(10) :: scores
+integer :: i, current_score
+character(len=1) :: grade
+character(len=11) :: remark
+
+!Initialize the aray with the provided scores
+scores= (/85, 62, 45, 91, 38, 74, 55, 88, 61, 47/)
+!Print Report Header
+print*, "================================================================"
+print*, "                   ACADEMIC PERFORMANCE REPORT                  "
+print*, "================================================================"
+print '(A15, A12, A12, A15)', "Student No", "Score", "Grade", "Remark"
+print*, "________________________________________________________________"
+
+!(c) Do loop and formatted output
 do i= 1,10
-if (mark>=80) .and. (mark<=100)
-print*, 'Distinction'
-else if (mark>=60).and. (mark<=79)
-print*, 'Credit'
-else if (mark>=40) .and. (mark<=59)
-print*, 'Pass'
-else if (mark>=0).and. (mark<=39)
-print*, 'Fail'
+current_score = scores(i)
+
+!(b) Grade classififcation using IF THEN/ELSE IF construct
+if (current_score>=80 .and. current_score<=100) then
+grade = 'A'
+remark = 'Distinction'
+else if (current_score>=60 .and. current_score<=79) then
+grade= 'B'
+remark = 'Credit'
+else if (current_score>=40 .and. current_score<=59) then
+grade = 'C'
+remark = 'Pass'
+else
+grade = 'F'
+remark = 'Fail'
 end if
-call,dimension(10):: s
-print*,'s'
-end program grade
+
+!Print a formatted line of output for each student
+print '(I10,I16,A11,A19)', i, current_score, grade, remark
+end do
+print*, "================================================================"
+
+!Format statement for aligned output 
+100 format(I17, I12, A10, A17)
+end program grading_system
 
