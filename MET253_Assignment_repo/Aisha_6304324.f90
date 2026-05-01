3df171893b310a1b3b890d8572d381b12e60b02f
Aisha AbdulAziz <aishaabdulaziz778@gmail.com>
2026-05-01 21:50:59 +0000

    Update Aisha_6304324.f90

--- MET253_Assignment_repo/Aisha_6304324.f90
+++ MET253_Assignment_repo/Aisha_6304324.f90
@@ -1,29 +1,26 @@
     PROGRAM student_grade
         IMPLICIT NONE
-        INTEGER(DIMENSION=10)::85,62,45,91,38,74,55,88,61,47
-    INTEGER:: i
-        PRINT*,'STUDENT SCORE MARK: "%"'
+    !declaration of variables
+        INTEGER(DIMENSION=10)::score
+    INTEGER:: i,current_score
+    score =(/ 85,62,45,91,38,74,55,88,61,47/)
+        PRINT*,'STUDENT SCORE MARK AND GRADES :'
 do i = 1,10
-i(1)=85
-i(2)=62
-i(3)=45
-i(4)=91
-i(5)=38
-i(6)=74
-i(7)=55
-i(8)=88
-i(9)=61
-i(10)=47
-end do
-IF ('(score>=80).AND.(score>=100)')THEN
-    PRINT*,'A'
-IF('(score>=60).AND.(score>=79)')THEN
-    PRINT*,'B'
-IF('(score>=40).AND.(score>=59)')THEN
-    PRINT*,'C'
-IF('(score>=0).AND.(score>=39)')THEN
+!assigning values
+current_score=score(i)
+!starting of the the IF and do loops 
+IF (current_score>=80) THEN
+    PRINT*,current_score,': GRADE A'
+ELSE IF(current_score>=60)THEN
+    PRINT*,current_score,' : GRADE B'
+ELSE IF(current_score>=40)THEN
+    PRINT*,current_score,': GRADE C'
+    !if the score is below 40 the grade d is given
+    ELSE
+PRINT*,current_score,':GRADE D'
     PRINT*,'D'
 END IF 
+end do
 !6304324
 END PROGRAM student_grade
 
