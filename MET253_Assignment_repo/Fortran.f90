dd4168e254d84cd9edd6820e3fc491d6d3239550
GerrardBoakyeAgyeman <gerrardboakyeagyeman@gmail.com>
2026-05-01 20:13:31 +0000

    Update Fortran.f90

--- MET253_Assignment_repo/Fortran.f90
+++ MET253_Assignment_repo/Fortran.f90
@@ -1,17 +1,21 @@
 !Index Number - 6308524
 Program Quiz
  IMPLICIT NONE
-
+ 
+ ! Declaration of data
  INTEGER, DIMENSION(10) :: scores = (/85, 62, 45, 91, 38, 74, 55, 88, 61, 47/)
  INTEGER :: i, score
  CHARACTER(LEN=1) :: grade
  CHARACTER(LEN=11) :: remark
 
+ ! This is for the heading of the table
  PRINT "(A6, A10, A8, A13)", "Std No", "Score", "Grade", "Remark"
 
+ ! Do loop
    DO i = 1, 10
      score = scores(i)
 
+        ! This is to classify the grades
         IF (score >= 80 .AND. score <= 100) THEN
             grade  = "A"
             remark = "Distinction"
@@ -26,6 +30,7 @@ Program Quiz
             remark = "Fail"
         END IF
 
+        ! Formatted output for each studen
         PRINT "(I6, I10, A8, A13)", i, score, grade, remark
 
    END DO
