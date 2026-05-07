fb0ceb31f4bb5ea9750ff8f8dc65f96c0c33781e
michellefosuah <michellefosuah17@gmail.com>
2026-05-08 08:10:32 +1300

    updated Michelle fosuah kwarteng 6310424

--- MET253_Assignment_repo/Test.f90
+++ MET253_Assignment_repo/Test.f90
@@ -7,7 +7,7 @@ CHARACTER(LEN=12) :: Remarks
 scores = (/85,62,45,91,38,74,55,88,61,47/)!I declared the scores in this form instead of the vertical order
 PRINT'(3X,A8,3X,A10,A15)', 'Scores','Grades','Remarks'
 DO i = 1,10
-   IF (scores(i) >= 80 .AND. scores(i)<=100) THEN
+   IF (scores(i) >= 80 .AND. scores(i)<=100) THEN ! I fixed the if statments
       Grade = 'A'
       Remarks = 'Distinction'
    ELSE IF (scores(i) >= 60 .AND. scores(i)<=79) THEN
