866720c47355780dd8766f5d7e6ce9ebbe69b080
michellefosuah <michellefosuah17@gmail.com>
2026-05-08 08:08:04 +1300

    updated Michelle fosuah kwarteng 6310424
    Updated comments for clarity and formatted output.

--- MET253_Assignment_repo/Test.f90
+++ MET253_Assignment_repo/Test.f90
@@ -1,10 +1,10 @@
 PROGRAM Exams_score
 IMPLICIT NONE
-INTEGER, DIMENSION(10) :: scores
+INTEGER, DIMENSION(10) :: scores! i changed the initial variable fro A to scores
 INTEGER :: i
 CHARACTER(LEN=1) :: Grade
 CHARACTER(LEN=12) :: Remarks
-scores = (/85,62,45,91,38,74,55,88,61,47/)
+scores = (/85,62,45,91,38,74,55,88,61,47/)!I declared the scores in this form instead of the vertical order
 PRINT'(3X,A8,3X,A10,A15)', 'Scores','Grades','Remarks'
 DO i = 1,10
    IF (scores(i) >= 80 .AND. scores(i)<=100) THEN
@@ -20,6 +20,6 @@ DO i = 1,10
       Grade = 'F'
       Remarks = 'Fail'
    END IF
-   PRINT '(I10,A10,5X,A15)',scores(i), Grade, Remarks
+   PRINT '(I10,A10,5X,A15)',scores(i), Grade, Remarks ! I formatted theoutput using the formatting descriptors
    END DO
 END PROGRAM Exams_score
