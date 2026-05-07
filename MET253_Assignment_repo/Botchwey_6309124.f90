cdc135fb5a015cef2136342ceee4e31cf89a743c
rkayb <randybotchwey46@gmail.com>
2026-05-07 23:11:40 +0000

    Refactor score handling and add grading system
    Refactored the program to use arrays for scores and added variables for grade and remark. Implemented a loop to process and display student scores along with their corresponding grades and remarks.

--- MET253_Assignment_repo/Botchwey_6309124.f90
+++ MET253_Assignment_repo/Botchwey_6309124.f90
@@ -1,27 +1,30 @@
 Program Midsem
 Implicit None
-Integer, Dimension(10) :: a,i
-INTEGER :: score
-a(1) = 85
-a(2) = 62
-a(3) = 45
-a(4) = 91
-a(5) = 38
-a(6) = 74
-a(7) = 55
-a(8) = 88
-a(9) = 61
-a(10) = 47
-
-IF (score >= 80) THEN
-Print*, 'A : Distiction'
-ELSE IF (score >=60) THEN
-Print*, 'B : Credit'
-ELSE IF (score >=40) THEN
-Print*, 'C : Pass'
-ELSE IF (score <=39) THEN
-Print*, 'D : Fail'
+!I'm adding new variables to store the remarks and grade and also changing the score numbers to the shortform
+Integer, Dimension(10) :: score = (/85, 62, 45, 91, 38, 74, 55, 88, 61, 47/)
+INTEGER :: i
+Character(len=1) :: grade
+Character(len=12) :: remark
+!This will print out the heading of the data
+Print*, 'Student   Score   Grade   Remark'
+!I'm adding a Do loop to loop through all the scores 
+Do i = 1, 10
+!Instead of printing the output immediately, I'll store them in the grade and remark variables and print them later together
+IF (score(i) >= 80) THEN
+grade = 'A'
+remark = 'Distiction'
+ELSE IF (score(i) >=60) THEN
+grade ='B'
+remark = 'Credit'
+ELSE IF (score(i) >=40) THEN
+grade ='C'
+remark = 'Pass'
+ELSE IF (score(i) <=39) THEN
+grade = 'D' 
+remark = 'Fail'
 END IF
-
+!Now I'm printing the ouput together here and also formating it for nice arrangement
+Print'(I5, I9, A8, 5x, A12)', i, score(i), grade, remark
+End Do
 End Program Midsem
 !Index number 6309124
