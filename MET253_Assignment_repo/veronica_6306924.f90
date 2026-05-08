8db34a7efd5c42e0dd7aca65ea9502dc8fc016d9
AVeronica-art <amponsahveronica340@gmail.com>
2026-05-08 13:15:40 +0000

    Update veronica_6306924.f90

--- MET253_Assignment_repo/veronica_6306924.f90
+++ MET253_Assignment_repo/veronica_6306924.f90
@@ -2,25 +2,26 @@ PROGRAM veronica
 !Amponsah Veronica
 !6306924
 IMPLICIT NONE
-REAL,demension(10::b
-INTEGER::5,5
-DO i=1,6
 INTEGER::i
-REAL::A,B,C,F
-!b=(/85,65,45,91,38,74,55,88,61,47/)
-DOi=(1,10)
-READ*,b
+REAL, DIMENSION (10)::z =(/85,62,45,91,38,74,55,88,61,47/) !I changed the B to z since the question asked that I used B for results under credit 
+CHARACTER (LEN12)::A,B,C,F, DISTINCTION, CREDIT, PASS,FAIL
+PRINT*,'enter 10 results'
+DO i=1,10
+READ*,z(i)
 END DO
-IF(100>=b).AND.(b>=80)THEN
-PRINT*A'distintion'
-ELSE,
-IF(79>=b).AND.(b>=60)THEN
-PRINT*B,'credit'
-ELSE,
-IF(59>=b).AND.(>=40)THEN
-PRINT*C,'PASS'
-ELSE,
-IF(b<=49)THEN[
-PRINT*F,'FAIL'
-END IF
+DOi=1,10
+IF((z(i)>=80.0).AND.(z(i)<=100.0)) THEN
+PRINT*,'A',z(i),'DISTINCTION'
+ELSE IF((z(i)>=60.0).AND.((z(i)<=79.0)) THEN
+PRINT*,'B',z(i),'CREDIT'
+ELSE IF((z(i)>=40.0).AND (z(i)<=59.0)) THEN
+PRINT*,'C'z(i),'PASS'
+ELSE IF((z(i)>=0.0).AND.(z(i)<=39.0)) THEN
+PRINT*,'F',z(i),'FAIL'
+ELSE
+PRINT*, 'ERROR'! used this for invalid scores 
+END IF ! I removed some brackets from the else if statement to ensure there is no error when compiling
+END DO
 END PROGRAM veronica
+
+
