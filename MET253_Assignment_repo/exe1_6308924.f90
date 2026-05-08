12e19113fed21b53876547f9bd5f13a932cc8b70
georginaboateng <monneydaniel7@gmail.com>
2026-05-08 19:35:30 +0000

    Update exe1_6308924.f90

--- MET253_Assignment_repo/exe1_6308924.f90
+++ MET253_Assignment_repo/exe1_6308924.f90
@@ -1,21 +1,25 @@
-Program grading student
+Program grading_student !program name should not have spaces
 Implicit none
      !6308924
-Integer ::SCORE RANGE
-IF (80<=100 .AND. 100<)then
-PRINT: ,'GRADE A',
-ELSE PRINT:'Distinction'
-IF (60<=79 .AND. 79,<)THEN
-PRINT: ,'GRADE B'
-ELSE PRINT:'CREDIT'
-IF (40<=59 .AND. 59<)THEN
-PRINT: ,'GRADE C'
-ELSE PRINT:'PASS'
-IF (0<=39 .AND. 39<)THEN
-PRINT: ,'GRADE F'
-ELSE PRINT:'FAIL'
-END PROGRAM grading student
+Integer ::SCORE    ! Proper variable declaration (no spaces like SCORE RANGE)
+PRINT *, "Enter score:"        ! ADDED: Needed input since no values were provided , also adding asterisk and comma to the print command
+READ *, score
+! CORRECT IF STRUCTURE
+IF (score >= 80 .AND. score <= 100) THEN     ! Correct logical condition 
+    PRINT *, "GRADE A"
+    PRINT *, "Distinction"
+ELSE IF (score >= 60 .AND. score <= 79) THEN  !Use ELSE IF (not separate IF)
+    PRINT *, "GRADE B"
+    PRINT *, "Credit"
+ELSE IF (score >= 40 .AND. score <= 59) THEN
+    PRINT *, "GRADE C"
+    PRINT *, "Pass"
+ELSE IF (score >= 0 .AND. score <= 39) THEN
+    PRINT *, "GRADE F"
+    PRINT *, "Fail"
+ELSE
+    PRINT *, "Invalid score"   ! ADDED: Handles wrong inputs
+END IF                         ! Must close IF block properly
+END PROGRAM grading_student
 
-PROGGRAM exam
-Implicit none
-REAL:: (1,10)
+
