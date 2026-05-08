0db99ef7902c441f7ea58c6060e14e76b48d4a21
derickkofiopareagyei-stack <derickkofiopareagyei@gmail.com>
2026-05-08 15:30:43 +0000

    Update DERICK_6312224.f90

--- MET253_Assignment_repo/DERICK_6312224.f90
+++ MET253_Assignment_repo/DERICK_6312224.f90
@@ -1,31 +1,29 @@
-!iINDEX NUMBER:6312224
+!INDEX NUMBER:6312224
 !NAME:OPARE-AGYEI DERICK
-PROGRAM DERICK
-IMPLICIT NONE
-INTEGER,DIMENSION(10)::a
-INTEGER,DIMENSION(10)::s
-a(1)=1 ; s(1)=85
-a(2)=2 ; s(2)=62
-a(3)=3 ; s(3)=45
-a(4)=4 ; s(4)=91
-a(5)=5 ; s(5)=30
-a(6)=6 ; s(6)=74
-a(7)=7 ; s(7)=55
-a(8)=8 ; s(8)=88
-a(9)=9 ; s(9)=61
-a(10)=10 ; s(10)=47
- do row=(1,2)
-do count*,(1,10)
-
-
-PRINT*,'Enter the score'
-READ*,s
-IF(a>=100)THEN
-PRINT*,'GRADE A'
-ELSE IF(s>=79)THEN
-PRINT*,'GRADE B'
-ELSE IF(s>=59)THEN
-PRINT*,'GRADE C'
-ELSE IF(s<39)THEN
-PRINT*,'GRADE F'
-PRINT'
+PROGRAM DERICK  !Name of the program
+IMPLICIT NONE   !Force all variables to be declared explicitly
+INTEGER,DIMENSION(10)::scores=(/85,62,45,91,38,74,55,88,61,47/) !Declaring an integer array of 10 scores
+INTEGER::i   !Loop counter variable 
+CHARACTER(LEN=1)::grade  !Variable to store student's grade
+CHARACTER(LEN=12)::remark !Variable to store student's remark
+PRINT*,"....................................."  !Prints a decorative line
+PRINT*,"STUDENT NO.  SCORE  GRADE  REMARK"          !Prints table headings
+PRINT*,"....................................."
+DO i=1,10                                       !Loop through all the 10 students' scores
+IF (scores(i) >=80 .AND. scores(i) <=100)THEN   !IF statement to determine the grade and remarks of the scores' of the students
+grade='A'
+remark='Distinction'
+ELSE IF (scores(i) >=60 .AND. scores(i) <=79)THEN
+grade='B'
+remark='Credit'
+ELSE IF (scores(i) >=40 .AND. scores(i) <=59)THEN
+grade='C'
+remark='Pass'
+ELSE
+grade='F'
+remark='Fail'
+END IF                                     !End of the IF statement
+WRITE(*,26) i , scores(i) , grade , remark  !Outputs student's number,score,grade and remark
+END DO                                     !End the DO loop
+26 FORMAT (I5,5X,I5,5X,A1,7X,A12)           !Defines the output formatting
+END PROGRAM DERICK                         !End of the program
