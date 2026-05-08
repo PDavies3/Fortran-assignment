f29d84650d7300fc4926d9c0c8afb99da7043992
vpjmwprjb8-alt <vpjmwprjb8@privaterelay.appleid.com>
2026-05-08 05:48:28 -0700

    Update Monica_6306724.f90

--- MET253_Assignment_repo/Monica_6306724.f90
+++ MET253_Assignment_repo/Monica_6306724.f90
@@ -1,6 +1,5 @@
 !Amoh Monica Aboagyewaa 6306724
 PROGRAM mid_sem
-
  IMPLICIT NONE
   INTEGER :: i
   REAL, DIMENSION(10)::R     !R=RESULTS OF STUDENTS
@@ -17,16 +16,21 @@ PROGRAM mid_sem
    
    IF ((R(i)>=80.00).AND.(R(i)<=100.00)) THEN
     PRINT*,R(i),'A', '  ','DISTINCTION'
+   
    ELSE IF ((R(i)>=60.00).AND.(R(i)<=79.00)) THEN
     PRINT*,R(i),'B','  ','CREDIT'
+   
    ELSE IF ((R(i)>=40.00).AND.(R(i)<=59.00)) THEN
     PRINT*,R(i),'C','  ','PASS'
+   
    ELSE IF ((R(i)>=0.00).AND.(R(i)<=39.99)) THEN
     PRINT*,R(i),'F','  ','FAIL'
+   
    ELSE 
     PRINT*,'ERROR:  INVALID SCORE' 
+   
    END IF
    
   END DO   
-  
+ 
 END PROGRAM mid_sem
